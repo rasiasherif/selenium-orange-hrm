817afd98ff96d9ca0322d6afce507c89292484df
rasiasherif <72366837+rasiasherif@users.noreply.github.com>
2022-12-17 14:22:17 -0600

    Add files via upload
    orange hrm automation

---
+++ project_orangehrm/Test_Codes/TestHRM.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from Test_Data import Data
+import pytest
+import time
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+# cap = DesiredCapabilities().FIREFOX
+# cap["marionette"] = False
+
+class Test_OrangeHrm:
+    url = "https://opensource-demo.orangehrmlive.com"
+    
+    # Booting Method for running the Python Tests
+    @pytest.fixture
+    def booting_function(self):
+        self.driver = webdriver.Edge()
+        yield
+        self.driver.close()
+    
+    def test_get_title(self, booting_function):
+        self.driver.get(self.url)
+        assert self.driver.title == 'OrangeHRM'
+        print("SUCCESS # Web Title Captured Successfully")
+    
+    def test_login(self, booting_function):
+        self.driver.get(self.url)
+        time.sleep(5)
+        self.driver.find_element(by=By.NAME, value=Data.HRM_Selectors.input_box_username).send_keys(Data.HRM_Data.username)
+        self.driver.find_element(by=By.NAME, value=Data.HRM_Selectors.input_box_password).send_keys(Data.HRM_Data.password)
+        self.driver.find_element(by=By.XPATH, value=Data.HRM_Selectors.login_xpath).click()
+        assert self.driver.title == 'OrangeHRM'
+        print("SUCCESS # LOGGED IN WITH USERNAME {username} and PASSWORD {password}".format(username=Data.HRM_Data.username, password=Data.HRM_Data.password))
---
+++ project_orangehrm/Test_Data/Data.py
@@ -0,0 +1,10 @@
+class HRM_Data:
+    username = "Admin"
+    password = "admin123"
+
+# Python Class for Selenium Selectors
+class HRM_Selectors:
+    input_box_username = "username"
+    input_box_password = "password"
+    login_xpath = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+
