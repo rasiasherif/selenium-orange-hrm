d7f4a4061b809fc6b960deb28df18449367508c5
rasiasherif <72366837+rasiasherif@users.noreply.github.com>
2022-12-17 14:57:47 -0600

    Add files via upload
    invalid login test

--- project_orangehrm/Test_Codes/TestHRM.py
+++ project_orangehrm/Test_Codes/TestHRM.py
@@ -31,3 +31,13 @@ class Test_OrangeHrm:
         self.driver.find_element(by=By.XPATH, value=Data.HRM_Selectors.login_xpath).click()
         assert self.driver.title == 'OrangeHRM'
         print("SUCCESS # LOGGED IN WITH USERNAME {username} and PASSWORD {password}".format(username=Data.HRM_Data.username, password=Data.HRM_Data.password))
+    
+    def test_invalid_login(self, booting_function):
+        self.driver.get(self.url)
+        time.sleep(5)
+        self.driver.find_element(by=By.NAME, value=Data.HRM_Selectors.input_box_username).send_keys(Data.HRM_Data.username)
+        self.driver.find_element(by=By.NAME, value=Data.HRM_Selectors.input_box_password).send_keys(Data.HRM_Data.invalid_password)
+        self.driver.find_element(by=By.XPATH, value=Data.HRM_Selectors.login_xpath).click()
+        time.sleep(5)
+        assert self.driver.find_element(by=By.XPATH, value=Data.HRM_Selectors.login_err_msg_xpath).text == 'Invalid credentials'
+        print("FAILED # LOGIN FAILED WITH USERNAME {username} and INVALID PASSWORD {password}".format(username=Data.HRM_Data.username, password=Data.HRM_Data.invalid_password))
--- project_orangehrm/Test_Data/Data.py
+++ project_orangehrm/Test_Data/Data.py
@@ -1,10 +1,12 @@
 class HRM_Data:
     username = "Admin"
     password = "admin123"
+    invalid_password = "wrongpassword"
 
 # Python Class for Selenium Selectors
 class HRM_Selectors:
     input_box_username = "username"
     input_box_password = "password"
     login_xpath = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+    login_err_msg_xpath = '/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p'
 
